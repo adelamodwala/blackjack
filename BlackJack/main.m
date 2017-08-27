ca17cc7c70808fc5eab861b8fb9476d770a09982
Adel Amodwala <adel.amodwala@gmail.com>
2017-08-26 21:49:32 -0400

    Initial Commit

---
+++ BlackJack/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlackJack
+//
+//  Created by Adel Amodwala on 2017-08-26.
+//  Copyright © 2017 Adel Amodwala. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
